d9ea7d93fe82e129e75e68c7b5a078cfe9778a48
Ayush9863 <ayushrao299@gmail.com>
2026-02-08 00:30:09 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,100 @@
+from cryptography.fernet import Fernet
+import hashlib
+import os
+
+# Encryption key generate karo (ek baar)
+# key = Fernet.generate_key()
+# with open("key.key","wb") as f:
+#     f.write(key)
+
+def load_key():
+    with open("key.key", "rb") as f:
+        return f.read()                   
+
+key=load_key()
+fer=Fernet(key)   #iska mtlb=“Is key se ek encryption/decryption tool bana do” 
+# fer.encrypt() → lock
+# fer.decrypt() → unlock
+
+# 👉 Fernet = lock + unlock machine
+# Iske bina password encrypt/decrypt nahi hoga.   
+
+
+def set_master_password():
+    master_pwd=input("what is the master password")
+    hashed = hashlib.sha256(master_pwd.encode()).hexdigest()
+    with open("master.key", "w") as f:
+        f.write(hashed)
+    print("Master password set successfully 🔐")
+
+def verify_master_password():
+    if not os.path.exists("master.key"):
+        set_master_password()
+# "Agar master.key file EXIST NAHI karti, to naya master password set karwa lo."
+    with open("master.key", "r") as f:
+        saved_hash = f.read()
+# saved_hash = file me saved password ka hash
+# 👉 Real password kabhi store nahi hota
+# 👉 Sirf uska hash (random looking string) store hota hai
+    attempts = 3
+    while attempts > 0:
+        master_pwd = input("Enter master password: ")
+        hashed = hashlib.sha256(master_pwd.encode()).hexdigest()
+
+        if hashed == saved_hash:
+            print("Access granted ✅\n")
+            return True
+        else:
+            attempts -= 1
+            print(f"Wrong password ❌ | Attempts left: {attempts}")
+
+    print("⛔ Too many wrong attempts. Program locked.")
+    return False
+
+
+def view():
+    with open("passwords.txt","r") as f:
+        for line in f.readlines():
+            data=line.strip()
+            user,passw=data.split("|")
+            print("User:",user,"|Password:",fer.decrypt(passw.encode()).decode())
+       
+def add():
+    name=input("Account Name:")
+    pwd=input("password:")
+    with open("passwords.txt","a") as f:
+         f.write(name+"|"+fer.encrypt(pwd.encode()).decode()+"\n")
+
+def delete():
+    name = input("Account name to delete: ")
+    lines = []
+    found = False
+    with open("passwords.txt", "r") as f:
+        lines = f.readlines()
+    with open("passwords.txt", "w") as f:
+        for line in lines:
+            if name + "|" not in line:
+                f.write(line)
+            else:
+                found = True
+    if found:
+        print(f"{name} deleted successfully")
+    else:
+        print("Not found")         
+
+# .encode() → string ➜ bytes
+# .decode() → bytes ➜ string
+# Python me Fernet (encryption) only bytes accept karta hai, isliye convert karna padta hai.         
+
+if not verify_master_password():
+    exit()
+while True:
+    mode = input("would you like to add a new password or view existing ones or delete ones(view,add,delete),press q to quit").lower()
+    if mode=="q":
+        break
+    if mode=="view":
+       view()
+    elif mode=="add":
+        add()
+    elif mode=="delete":
+        delete()    
